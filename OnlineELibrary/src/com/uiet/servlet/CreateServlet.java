72af921d917e1894bfe27e1025c4f8bccc75d4d1
Pradeep_Jaiswal <pradeepjaiswalsidd@gmail.com>
2021-01-14 05:55:53 +0530

    variable name corrected

--- OnlineELibrary/src/com/uiet/servlet/CreateServlet.java
+++ OnlineELibrary/src/com/uiet/servlet/CreateServlet.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
 public class CreateServlet extends HttpServlet {
 	
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
