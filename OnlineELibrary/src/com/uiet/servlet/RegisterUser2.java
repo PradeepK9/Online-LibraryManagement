a0a9c7c6ec9b2295cbf15b2b1b19738969131b1b
Pradeep_Jaiswal <pradeepjaiswalsidd@gmail.com>
2021-01-14 07:47:09 +0530

    book details servlet added

--- OnlineELibrary/src/com/uiet/servlet/RegisterUser2.java
+++ OnlineELibrary/src/com/uiet/servlet/RegisterUser2.java
@@ -1,26 +1,28 @@
 package com.uiet.servlet;
+
 import java.sql.*;
+
 public class RegisterUser2 {
-static int status=0;
-//int accountno=1;
-public static int register1(String bookname,String authorname){
-	
-	Connection con=GetCon.getCon();
-	PreparedStatement ps;
-	try {
-		ps = con.prepareStatement("Insert into BOOKDETAILS values(?,?,?)");
-		int	nextvalue1=GetCon.getPrimaryKey();
-	 	ps.setInt(1,nextvalue1);
-	    ps.setString(2,bookname);
-		ps.setString(3,authorname);
-			
-		status=ps.executeUpdate();
-		
-	} catch (SQLException e) {
-		
-		e.printStackTrace();
+	static int status = 0;
+
+	public static int register1(String bookname, String authorname) {
+
+		Connection con = GetCon.getCon();
+		PreparedStatement ps;
+		try {
+			ps = con.prepareStatement("Insert into BOOKDETAILS values(?,?,?)");
+			int nextvalue1 = GetCon.getPrimaryKey();
+			ps.setInt(1, nextvalue1);
+			ps.setString(2, bookname);
+			ps.setString(3, authorname);
+
+			status = ps.executeUpdate();
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		return status;
+
 	}
-	return status;
-	
-}
 }
